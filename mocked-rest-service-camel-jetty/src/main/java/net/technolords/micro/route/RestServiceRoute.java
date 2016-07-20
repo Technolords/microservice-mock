8f221fb174aec75605550f70823e9d6f62d6349b
Technolords <mike@technolords.net>
2016-07-21 00:25:22 +0200

    Changes main route so it accepts all requests (regardless of URI and type, such as GET or POST)

--- mocked-rest-service-camel-jetty/src/main/java/net/technolords/micro/route/RestServiceRoute.java
+++ mocked-rest-service-camel-jetty/src/main/java/net/technolords/micro/route/RestServiceRoute.java
@@ -1,67 +1,68 @@
 package net.technolords.micro.route;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
-
 import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
 import org.apache.camel.LoggingLevel;
+import org.apache.camel.Processor;
 import org.apache.camel.builder.RouteBuilder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import net.technolords.micro.processor.ResponseProcessor;
 
 /**
  * Created by Technolords on 2016-Jun-23.
  */
 public class RestServiceRoute extends RouteBuilder {
-    private static final Logger LOGGER = LoggerFactory.getLogger(RestServiceRoute.class);
+    private final Logger LOGGER = LoggerFactory.getLogger(getClass());
     private static final String DIRECT_MAIN = "direct:main";
-//    private static final String REST_PATH = "/mapng/traxis-structure-service/v1/pl/pl/vodStructure";
-    private static final String REST_PATH = "/mock";
-    private static final String PATH_TO_MOCKED_RESPONSE = "mock/remote-traxis-structure-service.json";
+    private static final String JETTY_MAIN = "jetty:http://";
+    private static final String JETTY_BINDING_ADDRESS = "0.0.0.0";
+    private static final String JETTY_BINDING_PATH = "/";
+    private static final String JETTY_OPTIONS = "?matchOnUriPrefix=true&enableJmx=true";
     private String port;
-    private String cachedResponse;
+    private Processor responseProcessor = new ResponseProcessor();
 
     public RestServiceRoute(String myPort) {
         this.port = myPort;
         LOGGER.info("Using port: " + this.port);
     }
 
+    /**
+     * Generates a Camel route, that listens from any HTTP request made (GET or POST) regardless
+     * of the path. The response resolution is delegated towards the response processor.
+     */
     @Override
     public void configure() throws Exception {
-        from("jetty:http://" + this.generateAddress())
+        onException(Exception.class)
+            .handled(true)
+            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(500))
+            .transform(simple("An error occurred: ${exception.message}"));
+
+        from(generateJettyEndpoint())
             .log(LoggingLevel.DEBUG, LOGGER, "Received request...")
             .setExchangePattern(ExchangePattern.InOut)
             .to(DIRECT_MAIN);
 
         from(DIRECT_MAIN)
-            .setHeader(Exchange.HTTP_RESPONSE_CODE, constant(200))
-            .setHeader("Content-Type", constant("application/json"))
-            .setBody(constant(this.generateResponse()));
-//            .setBody(constant("Demo"));
+            .log(LoggingLevel.DEBUG, LOGGER, "Current headers: ${headers}")
+            .log(LoggingLevel.DEBUG, LOGGER, "Current body: ${body}")
+            .process(this.responseProcessor);
     }
 
-    protected String generateAddress() {
+    /**
+     * Generates a Camel Jetty endpoint that looks like this:
+     *
+     *  "jetty:http://0.0.0.0:9090/?matchOnUriPrefix=true&enableJmx=true
+     *
+     * @return
+     *  A Camel Jetty endpoint
+     */
+    protected String generateJettyEndpoint() {
         StringBuilder buffer = new StringBuilder();
-        // 0.0.0.0:9090
-        buffer.append("0.0.0.0").append(":").append(this.port);
-        // /mapng/traxis-structure-service/v1/pl/pl/vodStructure
-        buffer.append(REST_PATH);
-        // Enable jmx support
-        buffer.append("?enableJmx=true");
+        buffer.append(JETTY_MAIN).append(JETTY_BINDING_ADDRESS).append(":").append(this.port);
+        buffer.append(JETTY_BINDING_PATH).append(JETTY_OPTIONS);
         return buffer.toString();
-    }
-
-    protected String generateResponse() throws IOException {
-        if (this.cachedResponse == null) {
-            InputStream fileStream = Thread.currentThread().getContextClassLoader().getResourceAsStream(PATH_TO_MOCKED_RESPONSE);
-            LOGGER.info("Path to mocked file exists: " + fileStream.available());
-            this.cachedResponse = new BufferedReader(new InputStreamReader(fileStream)).lines().collect(Collectors.joining("\n"));
-        }
-        return this.cachedResponse;
     }
 
 }
