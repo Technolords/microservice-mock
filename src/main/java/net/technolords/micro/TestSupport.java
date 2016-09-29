70a49bdbdcea5c8b8d68d8d9cbe5149e5b7d4c48
SrideviJava <jsridevi@gmail.com>
2016-09-29 21:11:54 +0200

    added test to verify ResponseProcessor class.

---
+++ src/main/java/net/technolords/micro/TestSupport.java
@@ -0,0 +1,79 @@
+package net.technolords.micro;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.annotations.BeforeClass;
+
+import java.io.File;
+import java.nio.file.FileSystems;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+/**
+ * Created by Technolords on 2015-Dec-04.
+ */
+public class TestSupport {
+    private static final Logger LOGGER = LoggerFactory.getLogger(TestSupport.class);
+    private Path pathToClassFolder;
+    private Path pathToDataFolder;
+    private Path pathToTargetFolder;
+
+    /**
+     * Since the artificer project is part of a multi module Maven build, the start point of executing the test
+     * can differ. I.e. from 'root pom' or from 'artificer pom' which means there are different path's to the
+     * test resources. This method fixes the correct location of the test resources by calculating the path.
+     */
+
+    /**
+     * Depending on the execution of the test files, whether from IDE or from multi module maven project (CLI),
+     * the target and data folders are relative. In order to overcome this, the folders are calculated.
+     */
+
+    @BeforeClass
+    public void configureRelativeFolders() {
+        // Create String which depends on file system (Unix vs Windows) by using the file separator
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("src").append(File.separator).append("test").append(File.separator);
+        buffer.append("resources").append(File.separator).append("data");
+
+        // Set path to folder containing the data, i.e. src/test/resources/data
+        Path pathToData = FileSystems.getDefault().getPath(buffer.toString());
+        if(!pathToData.toAbsolutePath().toString().contains("microservice-mock")) {
+            StringBuilder pathWithPrefix = new StringBuilder();
+            pathWithPrefix.append("artificer").append(File.separator).append(buffer.toString());
+            pathToData = FileSystems.getDefault().getPath(pathWithPrefix.toString());
+        }
+        this.pathToDataFolder = pathToData.toAbsolutePath();
+        LOGGER.debug("Data folder set: {} and exists: {}", this.pathToDataFolder.toString(), Files.exists(this.pathToDataFolder));
+
+        // Set path to Target folder to create Output, i.e. target
+        Path pathToTarget = FileSystems.getDefault().getPath("target");
+        if(!pathToTarget.toAbsolutePath().endsWith("artificer" + File.separator + "target")) {
+            pathToTarget = FileSystems.getDefault().getPath("artificer" + File.separator + "target");
+        }
+        this.pathToTargetFolder = pathToTarget.toAbsolutePath();
+        LOGGER.debug("Target folder set: {} and exists: {}", this.pathToTargetFolder.toString(), Files.exists(this.pathToTargetFolder));
+
+        // Set path to folder containing the data, i.e. src/test/resources/data/class
+        this.pathToClassFolder = FileSystems.getDefault().getPath(pathToData.toAbsolutePath() + File.separator + "class");
+        LOGGER.debug("Class folder set: {} and exists: {}", this.pathToClassFolder.toString(), Files.exists(this.pathToClassFolder));
+    }
+
+    /**
+     * Auxiliary method to get a reference of the class folder, as sub folder of the data folder.
+     *
+     * @return
+     *  The reference of the class folder.
+     */
+    public Path getPathToClassFolder() {
+        return this.pathToClassFolder;
+    }
+
+    public Path getPathToDataFolder() {
+        return this.pathToDataFolder;
+    }
+
+    public Path getPathToTargetFolder() {
+        return  this.pathToTargetFolder;
+    }
+}
--- src/test/java/net/technolords/micro/processor/ResponseProcessorTest.java
+++ src/test/java/net/technolords/micro/processor/ResponseProcessorTest.java
@@ -1,7 +1,6 @@
 package net.technolords.micro.processor;
 
-
-import junit.framework.Assert;
+import net.technolords.micro.TestSupport;
 import net.technolords.micro.config.ConfigurationManager;
 import org.apache.camel.Exchange;
 import org.apache.camel.impl.DefaultCamelContext;
@@ -10,49 +9,54 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
+import java.io.File;
+import java.nio.file.FileSystems;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
-public class ResponseProcessorTest {
+public class ResponseProcessorTest extends TestSupport{
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-
-    String PATH_TO_CONFIG_FILE = "D:/commit2Learn/data/mockConfigurations/";
-    String PATH_TO_REQUEST = "D:/commit2Learn/data/mockRequests/";
-    String PATH_TO_RESPONSE = "D:/commit2Learn/data/mockResponses/";
 
     @DataProvider(name = "dataSetMockExpectation")
     public Object[][] dataSetMock(){
         return new Object[][] {
-                {"configuration-test1.xml","sample-post1-request.xml", "sample-post1.txt."}
+                {"configuration-test1.xml","POST", "/mock/post", "sample-post1-request.xml", "sample-post1.txt."},
+                {"configuration-test1.xml","GET", "/mock/get",null, "sample-get.txt"}
         };
     }
 
     @Test(dataProvider = "dataSetMockExpectation")
-    public void testPostResponses(String configFile, String request, String expectedResponse) throws Exception {
+    public void testPostResponses(String configFile, String method, String URI, String request, String expectedResponse) throws Exception {
+        // Create a path to the file
+        Path pathToConfigFile = FileSystems.getDefault().getPath(getPathToDataFolder() + File.separator + "mockConfigurations" + File.separator);
+        Path pathToRequestFile = FileSystems.getDefault().getPath(getPathToDataFolder() + File.separator + "mockRequests");
+        Path pathToResponseFile = FileSystems.getDefault().getPath(getPathToDataFolder() + File.separator + "mockResponses");
 
-//        String expectedResponse = "{\n" +
-//                "  \"label \" : \"post-request1\",\n" +
-//                "  \"data\" : \"this is also fun\"\n" +
-//                "}";
+        String responseContent = new String(Files.readAllBytes(Paths.get(pathToResponseFile + File.separator + expectedResponse)));
 
-        String requestContent = new String(Files.readAllBytes(Paths.get(PATH_TO_REQUEST + request)));
-        String responseContent = new String(Files.readAllBytes(Paths.get(PATH_TO_RESPONSE + expectedResponse)));
 
-        String pathToConfig = PATH_TO_CONFIG_FILE + configFile;
+        String pathToConfig = pathToConfigFile + File.separator +configFile;
         ConfigurationManager configurationManager = new ConfigurationManager(pathToConfig, null);
 
         Exchange exchange = new DefaultExchange(new DefaultCamelContext());
-        exchange.getIn().setHeader(Exchange.HTTP_METHOD, "POST");
-        exchange.getIn().setHeader(Exchange.HTTP_URI, "/mock/post");
+        exchange.getIn().setHeader(Exchange.HTTP_METHOD, method);
+        exchange.getIn().setHeader(Exchange.HTTP_URI, URI);
+
+        if(request != null){
+        String requestContent = new String(Files.readAllBytes(Paths.get(pathToRequestFile + File.separator+ request)));
         exchange.getIn().setBody(requestContent);
+        }
+
 
         ResponseProcessor responseProcessor = new ResponseProcessor(configurationManager);
         responseProcessor.process(exchange);
 
         //Assertions
         String actualResponse = exchange.getIn().getBody(String.class);
-        Assert.assertEquals(actualResponse, expectedResponse);
+        Assert.assertEquals(actualResponse, responseContent);
 
 
     }
