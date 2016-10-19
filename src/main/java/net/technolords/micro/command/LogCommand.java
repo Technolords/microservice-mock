d3d5a858c6557c5dfbdafa6fb53b604d47ebeead
Technolords <mike@technolords.net>
2016-10-19 23:21:44 +0200

    Changed logging framework by replacing logback by log4j 2, as well as the associated files (logback.xml and log4j2.xml)

--- src/main/java/net/technolords/micro/command/LogCommand.java
+++ src/main/java/net/technolords/micro/command/LogCommand.java
@@ -2,11 +2,14 @@ package net.technolords.micro.command;
 
 import java.net.HttpURLConnection;
 
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.core.LoggerContext;
+import org.apache.logging.log4j.core.config.Configuration;
+import org.apache.logging.log4j.core.config.LoggerConfig;
+import org.apache.logging.log4j.spi.StandardLevel;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ch.qos.logback.classic.Level;
-import ch.qos.logback.classic.LoggerContext;
 import net.technolords.micro.ResponseContext;
 
 public class LogCommand {
@@ -14,35 +17,37 @@ public class LogCommand {
 
     public static ResponseContext executeCommand(String logLevel) {
         LOGGER.debug("Log command called, with log level {}", logLevel);
-        LoggerContext loggerContext = (LoggerContext) LoggerFactory.getILoggerFactory();
-        ch.qos.logback.classic.Logger rootLogger = loggerContext.getLogger("ROOT");
+        LoggerContext loggerContext = LoggerContext.getContext(false);
+        Configuration configuration = loggerContext.getConfiguration();
+        LoggerConfig rootLogger = configuration.getRootLogger();
         ResponseContext responseContext = new ResponseContext();
         responseContext.setContentType(ResponseContext.PLAIN_TEXT_CONTENT_TYPE);
         if (rootLogger != null) {
-            switch (Level.toLevel(logLevel, Level.INFO).toInt()) {
-                case Level.ERROR_INT:
+            switch (StandardLevel.getStandardLevel(Level.toLevel(logLevel, Level.INFO).intLevel())) {
+                case ERROR:
                     rootLogger.setLevel(Level.ERROR);
                     responseContext.setResponse("Log level changed to ERROR");
                     break;
-                case Level.WARN_INT:
+                case WARN:
                     rootLogger.setLevel(Level.WARN);
                     responseContext.setResponse("Log level changed to WARN");
                     break;
-                case Level.DEBUG_INT:
+                case DEBUG:
                     rootLogger.setLevel(Level.DEBUG);
                     responseContext.setResponse("Log level changed to DEBUG");
                     break;
-                case Level.INFO_INT:
+                case INFO:
                     rootLogger.setLevel(Level.INFO);
                     responseContext.setResponse("Log level changed to INFO");
                     break;
-                case Level.OFF_INT:
+                case OFF:
                     rootLogger.setLevel(Level.OFF);
                     responseContext.setResponse("Logging switched off");
                     break;
                 default:
                     responseContext.setResponse("Log level unchanged, unsupported level: " + logLevel);
             }
+            loggerContext.updateLoggers();
         } else {
             responseContext.setResponse("Unable to change log level, no ROOT logger found...");
             responseContext.setErrorCode(String.valueOf(HttpURLConnection.HTTP_INTERNAL_ERROR));
