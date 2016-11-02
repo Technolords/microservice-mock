bce7bdc8cf22f0b07913eaffcb16540f53db4dfe
SrideviJava <jsridevi@gmail.com>
2016-11-02 07:18:52 +0100

    - Added Test to StopCommand Class

--- src/test/java/net/technolords/micro/command/StopCommandTest.java
+++ src/test/java/net/technolords/micro/command/StopCommandTest.java
@@ -1,36 +1,64 @@
 package net.technolords.micro.command;
 
+import net.technolords.micro.MockedRestService;
 import net.technolords.micro.config.ConfigurationManager;
+import net.technolords.micro.route.RestServiceRoute;
 import org.apache.camel.Exchange;
 import org.apache.camel.ServiceStatus;
+import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.DefaultExchange;
+import org.apache.camel.testng.CamelTestSupport;
 import org.slf4j.Logger;
+import org.apache.camel.testng.CamelSpringTestSupport;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
 
-public class StopCommandTest{
+public class StopCommandTest extends CamelTestSupport {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
 
     @Test
-    public void testStopCommand() throws IOException{
+    public void testStopCommand() throws Exception{
         String method = ConfigurationManager.HTTP_GET;
         String uri = "/mock/cmd?stop=now";
-        Exchange exchange = this.generateExchange(method, uri);
+        Exchange exchange;
+
+//        MockedRestService mockedRestService = new MockedRestService();
+//        mockedRestService.start();
+//
+//        Assert.assertTrue(ServiceStatus.Started.isStarted());
+
+//       RouteBuilder routeBuilder = new RouteBuilder() {
+//           @Override
+//           public void configure() throws Exception {
+//               errorHandler(deadLetterChannel("mock:error"));
+//
+//               from("direct:a").to("direct:b");
+//           }
+//       };
+
+
+        exchange = this.generateExchange(method, uri);
 
         Assert.assertEquals(exchange.getContext().getStatus(), ServiceStatus.Stopped);
     }
 
-    private Exchange generateExchange(String method, String uri) throws IOException {
+    private Exchange generateExchange(String method, String uri) throws Exception {
         method = ConfigurationManager.HTTP_GET;
         uri = "/mock/cmd?stop=now";
         Exchange exchange = new DefaultExchange(new DefaultCamelContext());
+
+        context().addRoutes(new RestServiceRoute());
+        context().start();
+
+        Assert.assertTrue(ServiceStatus.Started.isStarted());
         exchange.getIn().setHeader(Exchange.HTTP_METHOD, method);
         exchange.getIn().setHeader(Exchange.HTTP_URI, uri);
         return exchange;
     }
-
 }
