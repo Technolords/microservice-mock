75e5a798170098542b541c73a1f2ddf42c2fd101
Mike Ahlers <mike@technolords.net>
2016-11-29 22:55:17 +0100

    Implementation of the config command (#32)

--- github/doc/commands.md
+++ github/doc/commands.md
@@ -1,17 +1,18 @@
 Back to [main](https://github.com/Technolords/microservice-mock).
 
 # Commands
-This following commands are supported:
+This following commands are supported (sorted by alphabet):
 
-* [stop](https://github.com/Technolords/microservice-mock/blob/master/github/doc/commands.md#stop)
+* [config](https://github.com/Technolords/microservice-mock/blob/master/github/doc/commands.md#config)
 * [log](https://github.com/Technolords/microservice-mock/blob/master/github/doc/commands.md#log)
-* [stats](https://github.com/Technolords/microservice-mock/blob/master/github/doc/commands.md#stats)
 * [reset](https://github.com/Technolords/microservice-mock/blob/master/github/doc/commands.md#reset)
+* [stats](https://github.com/Technolords/microservice-mock/blob/master/github/doc/commands.md#stats)
+* [stop](https://github.com/Technolords/microservice-mock/blob/master/github/doc/commands.md#stop)
 
-## Stop
-This command will stop the mock service, like:
+## Config
+This command will show the current config, like:
 
-    http://localhost:9090/mock/cmd?stop=now
+    http://localhost:9090/mock/cmd?config=current
     
 At this moment the value is irrelevant and can be anything.
 
@@ -28,6 +29,13 @@ At this moment the follow values are accepted:
 * debug
 * off
 
+## Reset
+This command will reset the statistics, like
+
+    http://localhost:9090/mock/cmd?reset=now
+    
+At this moment the value is irrelevant and can be anything.
+
 ## Stats
 This command will report the statistics, like
 
@@ -35,9 +43,9 @@ This command will report the statistics, like
     
 At this moment the value can be html or anything else. The latter will return a single line with statistics in a single line (as text)
 
-## Reset
-This command will reset the statistics, like
+## Stop
+This command will stop the mock service, like:
 
-    http://localhost:9090/mock/cmd?reset=now
+    http://localhost:9090/mock/cmd?stop=now
     
 At this moment the value is irrelevant and can be anything.
---
+++ src/main/java/net/technolords/micro/command/ConfigCommand.java
@@ -0,0 +1,51 @@
+package net.technolords.micro.command;
+
+import java.io.StringWriter;
+import java.net.HttpURLConnection;
+
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import net.technolords.micro.config.ConfigurationManager;
+import net.technolords.micro.domain.ResponseContext;
+import net.technolords.micro.domain.jaxb.Configurations;
+import net.technolords.micro.registry.MockRegistry;
+
+public class ConfigCommand {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ConfigCommand.class);
+    private static ConfigurationManager configurationManager;
+
+    /**
+     * Auxiliary method that executes the config command.
+     *
+     * @return
+     *  The result of the log command.
+     */
+    public static ResponseContext executeCommand() {
+        LOGGER.debug("Config command called");
+        if (configurationManager == null) {
+            configurationManager = MockRegistry.findConfigurationManager();
+        }
+        ResponseContext responseContext = new ResponseContext();
+        try {
+            responseContext.setContentType(ResponseContext.XML_CONTENT_TYPE);
+            responseContext.setResponse(marshallToXML(configurationManager.getConfigurations()));
+        } catch (JAXBException e) {
+            responseContext.setContentType(ResponseContext.PLAIN_TEXT_CONTENT_TYPE);
+            responseContext.setResponse(e.getMessage());
+            responseContext.setErrorCode(String.valueOf(HttpURLConnection.HTTP_INTERNAL_ERROR));
+        }
+        return responseContext;
+    }
+
+    private static String marshallToXML(Configurations configurations) throws JAXBException {
+        StringWriter stringWriter = new StringWriter();
+        Marshaller marshaller = JAXBContext.newInstance(Configurations.class).createMarshaller();
+        marshaller.marshal(configurations, stringWriter);
+        return stringWriter.toString();
+    }
+}
