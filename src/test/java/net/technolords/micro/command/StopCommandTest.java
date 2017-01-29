98c6d974dcace7ad764f964859b17cb27bf7ed18
SrideviJava <jsridevi@gmail.com>
2017-01-29 06:17:51 +0100

    Added a new LOGGER statement for Debugging.

--- src/test/java/net/technolords/micro/command/StopCommandTest.java
+++ src/test/java/net/technolords/micro/command/StopCommandTest.java
@@ -4,6 +4,7 @@ import net.technolords.micro.RouteTestSupport;
 import net.technolords.micro.config.ConfigurationManager;
 import org.apache.camel.Exchange;
 import org.apache.camel.ServiceStatus;
+import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.DefaultExchange;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,16 +20,19 @@ public class StopCommandTest extends RouteTestSupport {
         String method = ConfigurationManager.HTTP_GET;
         String uri = "/mock/cmd?stop=now";
         Exchange exchange = this.generateExchange(method, uri);
-        LOGGER.info("About to stop, current current context: {} -> started: {}", getProducerTemplate().getCamelContext().getName(), getMain().isStarted());
+        LOGGER.info("About to stop, current current context: {} -> started: {}", exchange.getContext().getName(), getMain().isStarted());
+//        LOGGER.info("About to stop, current current context: {} -> started: {}", getProducerTemplate().getCamelContext().getName(), getMain().isStarted());
         getProducerTemplate().send("direct:main", exchange);
         Thread.sleep(1000L);
-        LOGGER.info("Exchange send, current context: {} -> started: {}", getProducerTemplate().getCamelContext().getName(), exchange.getContext().getStatus());
+        LOGGER.info("Exchange send, current context: {} -> started: {}", exchange.getContext().getName(), exchange.getContext().getStatus());
+//        LOGGER.info("Exchange send, current context: {} -> started: {}", getProducerTemplate().getCamelContext().getName(), exchange.getContext().getStatus());
         Assert.assertEquals(exchange.getContext().getStatus(), ServiceStatus.Stopped);
     }
 
     private Exchange generateExchange(String method, String uri) throws Exception {
         method = ConfigurationManager.HTTP_GET;
         uri = "/mock/cmd";
+//        Exchange exchange = new DefaultExchange(new DefaultCamelContext());
         Exchange exchange = new DefaultExchange(getProducerTemplate().getCamelContext());
         exchange.getIn().setHeader(Exchange.HTTP_METHOD, method);
         exchange.getIn().setHeader(Exchange.HTTP_URI, uri);
