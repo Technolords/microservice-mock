f5f3bcd380986668b65dd77d3286afb6080cdf44
Technolords <mike@technolords.net>
2017-04-05 17:09:06 +0200

    Refactor of test where comparison of responses is now based on context type, and promoted test data towards a file

--- src/test/java/net/technolords/micro/command/CommandManagerTest.java
+++ src/test/java/net/technolords/micro/command/CommandManagerTest.java
@@ -1,64 +1,100 @@
 package net.technolords.micro.command;
 
-import net.technolords.micro.RouteTestSupport;
-import net.technolords.micro.config.ConfigurationManager;
-import net.technolords.micro.model.ResponseContext;
-import net.technolords.micro.model.jaxb.Configurations;
-import net.technolords.micro.registry.MockRegistry;
+import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.apache.camel.Exchange;
-import org.apache.camel.impl.DefaultCamelContext;
 import org.apache.camel.impl.DefaultExchange;
+import org.custommonkey.xmlunit.XMLUnit;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class CommandManagerTest extends RouteTestSupport{
+import net.technolords.micro.RouteTestSupport;
+import net.technolords.micro.config.ConfigurationManager;
+import net.technolords.micro.model.ResponseContext;
+import net.technolords.micro.test.PathSupport;
+
+public class CommandManagerTest extends RouteTestSupport {
     private final Logger LOGGER = LoggerFactory.getLogger(getClass());
-    private static final String DATASET_FOR_COMMAND_MANAGER = "datasetCommands";
-    ResponseContext responseContext = new ResponseContext();
-    CommandManager commandManager = new CommandManager();
+    private static final String DATA_SET_FOR_COMMAND_MANAGER = "dataSetForCommandManager";
+    private CommandManager commandManager;
 
+    @BeforeTest(description = "Initialize XMLUnit")
+    public void initializeXMLUnit() {
+        XMLUnit.setIgnoreWhitespace(true);
+        XMLUnit.setIgnoreAttributeOrder(true);
+        XMLUnit.setIgnoreComments(true);
+    }
 
-    @DataProvider(name = DATASET_FOR_COMMAND_MANAGER)
-    public Object[][] dataset() {
+    @BeforeMethod
+    public void initializeCommandManager() {
+        this.commandManager = new CommandManager();
+    }
+
+    /**
+     * Auxiliary method to declare a data set to support testing of the CommandManager. An entry is specified
+     * with four elements, each meaning:
+     *
+     *  [0] : The command
+     *  [1] : The expected context type
+     *  [2] : The expected response code
+     *  [3] : The expected message
+     *
+     * @return
+     *  The data set.
+     */
+    @DataProvider (name = DATA_SET_FOR_COMMAND_MANAGER)
+    public Object[][] dataSetForCommandManager() throws IOException {
         return new Object[][] {
-                {"unknown","text/plain","501", "Currently not supported"},
-                {"stop","text/plain",null,"Stopping the mock.."},
-//                {"config","application/xml",null,"<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?><ns2:configurations " +
-//                        "xmlns:ns2=\"http://xsd.technolords.net\"><configuration type=\"GET\" url=\"/mock/get\"><resource delay=\"0\" " +
-//                        "error-rate=\"0\">mock/sample-get.json</resource></configuration><configuration type=\"GET\" url=\"/mock/*/get\">" +
-//                        "<resource delay=\"0\" error-rate=\"0\">mock/sample-get.json</resource></configuration><configuration type=\"GET\" url=\"/mock/1/get\"><resource delay=\"0\" " +
-//                        "error-rate=\"0\">mock/sample-get.json</resource></configuration><configuration type=\"POST\" url=\"/mock/post\">" +
-//                        "<namespaces><namespace prefix=\"technolords\">urn:some:reference:1.0</namespace></namespaces><resource-groups><resource-group>" +
-//                        "<resource delay=\"0\" error-rate=\"0\">mock/sample-post1.json</resource><xpath>/technolords:sample/technolords:message[@id = '1']</xpath></resource-group><resource-group><resource delay=\"10000\" " +
-//                        "error-rate=\"0\">mock/sample-post2.json</resource><xpath>/technolords:sample/technolords:message[@id = '2']</xpath></resource-group><resource-group><resource delay=\"0\" " +
-//                        "error-code=\"206\" error-rate=\"50\">mock/sample-post3.json</resource><xpath>/technolords:sample/technolords:message[@id = '3']</xpath></resource-group><resource-group><resource " +
-//                        "content-type=\"text/plain\" delay=\"0\" error-rate=\"0\">mock/sample-post4.txt</resource><xpath>/technolords:sample/technolords:message[@id = '4']</xpath>" +
-//                        "</resource-group></resource-groups></configuration></ns2:configurations>"},
-                {"log","text/plain",null,"Log level changed to INFO"},
-                {"reset","text/plain",null,"Statistics has been reset"}
+                { "unknown", ResponseContext.PLAIN_TEXT_CONTENT_TYPE, "501", "Currently not supported" },
+                { "stop", ResponseContext.PLAIN_TEXT_CONTENT_TYPE, null, "Stopping the mock.." },
+                { "config", ResponseContext.XML_CONTENT_TYPE, null, expectedResponse() },
+                { "log", ResponseContext.PLAIN_TEXT_CONTENT_TYPE, null, "Log level changed to INFO" },
+                { "reset", ResponseContext.PLAIN_TEXT_CONTENT_TYPE, null, "Statistics has been reset" },
         };
     }
 
-    @Test(dataProvider = DATASET_FOR_COMMAND_MANAGER)
-    public void testCommandManager(final String command, final String contentType, final String errorCode, final String response) throws Exception{
-        ConfigurationManager configurationManager = MockRegistry.findConfigurationManager();
-        Configurations configurations = configurationManager.getConfigurations();
-//        StatisticsHandler statisticsHandler = MockRegistry.findStatisticsHandler();
+    private static String expectedResponse() throws IOException {
+        Path pathToRequestFile = PathSupport.getPathToTestConfigForMockResources();
+        Path pathToResource = Paths.get(pathToRequestFile.toString(), "config-for-CommandManagerTest.xml");
+        Assert.assertTrue(Files.exists(pathToResource));
+        return new String(Files.readAllBytes(pathToResource));
+    }
 
-        Exchange exchange = new DefaultExchange(new DefaultCamelContext());
+    @Test (dataProvider = DATA_SET_FOR_COMMAND_MANAGER)
+    public void testCommandManager(final String command, final String contentType, final String errorCode, final String response) throws Exception {
+        // Prepare
+        Exchange exchange = new DefaultExchange(super.context());
         exchange.getIn().setHeader(Exchange.HTTP_METHOD, ConfigurationManager.HTTP_GET);
         exchange.getIn().setHeader(Exchange.HTTP_URI, "/mock/get");
-        exchange.getIn().setHeader(command,contentType);
+        exchange.getIn().setHeader(command, contentType);
 
-        responseContext = commandManager.executeCommand(exchange);
+        // Execute
+        ResponseContext responseContext = this.commandManager.executeCommand(exchange);
         LOGGER.info("Response: {}" , responseContext.getResponse());
         LOGGER.info("Content Type: {}" , responseContext.getContentType());
         LOGGER.info("Error code: {}" , responseContext.getErrorCode());
 
-        Assert.assertEquals(responseContext.getResponse(), response);
+        // Assert (note we cannot use String compare when we have XML responses)
+        switch (contentType) {
+            case ResponseContext.XML_CONTENT_TYPE:
+                assertXMLEqual(response, responseContext.getResponse());
+                break;
+            case ResponseContext.PLAIN_TEXT_CONTENT_TYPE:
+                Assert.assertEquals(responseContext.getContentType(), contentType);
+                break;
+            default:
+                Assert.fail("Unsupported contextType: " + contentType + " -> unclear how to compare response versus expected...");
+        }
         Assert.assertEquals(responseContext.getErrorCode(), errorCode);
         Assert.assertEquals(responseContext.getContentType(), contentType);
     }
