b05b9f58d0bf6b6ee71b883198dd18caaa532352
Technolords <mike@technolords.net>
2017-04-05 17:09:36 +0200

    Update of test using PathSupport

--- src/test/java/net/technolords/micro/command/ConfigCommandTest.java
+++ src/test/java/net/technolords/micro/command/ConfigCommandTest.java
@@ -2,12 +2,12 @@ package net.technolords.micro.command;
 
 import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
 
-import java.io.File;
 import java.io.InputStream;
-import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.Unmarshaller;
@@ -23,6 +23,7 @@ import org.testng.annotations.Test;
 
 import net.technolords.micro.RouteTestSupport;
 import net.technolords.micro.config.ConfigurationManager;
+import net.technolords.micro.model.jaxb.Configuration;
 import net.technolords.micro.model.jaxb.Configurations;
 import net.technolords.micro.registry.MockRegistry;
 import net.technolords.micro.test.PathSupport;
@@ -30,29 +31,12 @@ import net.technolords.micro.test.PathSupport;
 public class ConfigCommandTest extends RouteTestSupport {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigCommandTest.class);
     private static final String DATA_SET_FOR_TEST_CONFIGURATIONS = "dataSetForTestConfigurations";
-    private Path pathToMockConfigurationsFolder;
 
     @BeforeTest (description = "Initialize XMLUnit")
     public void initializeXMLUnit() {
         XMLUnit.setIgnoreWhitespace(true);
         XMLUnit.setIgnoreAttributeOrder(true);
         XMLUnit.setIgnoreComments(true);
-    }
-
-    @BeforeTest (description = "Initialize configuration folder")
-    public void initializeConfigurationFolder() {
-        StringBuilder buffer = new StringBuilder();
-        buffer.append("src").append(File.separator);
-        buffer.append("test").append(File.separator);
-        buffer.append("resources").append(File.separator);
-        buffer.append("config");
-        this.pathToMockConfigurationsFolder = FileSystems.getDefault().getPath(buffer.toString());
-    }
-
-    @Test
-    public void testInitialization() {
-        LOGGER.info("About to test existence of folder: {}", this.pathToMockConfigurationsFolder);
-        Assert.assertTrue(Files.exists(this.pathToMockConfigurationsFolder), "Folder is expected to exist");
     }
 
     @Test
@@ -73,24 +57,30 @@ public class ConfigCommandTest extends RouteTestSupport {
         assertXMLEqual(expectedConfig, actualConfig);
     }
 
-    @DataProvider(name = DATA_SET_FOR_TEST_CONFIGURATIONS)
+    @DataProvider (name = DATA_SET_FOR_TEST_CONFIGURATIONS)
     public Object[][] dataSetConfigs(){
         return new Object[][] {
                 { "config-2-for-ConfigCommandTest.xml" },
         };
     }
 
-    @Test(dataProvider = DATA_SET_FOR_TEST_CONFIGURATIONS)
+    @Test (dataProvider = DATA_SET_FOR_TEST_CONFIGURATIONS)
     public void testWithLoadConfigurations(final String testConfigFile) throws Exception {
         LOGGER.debug("About to send Config command, total routes: {}", getProducerTemplate().getCamelContext().getRoutes().size());
+
         // Validate presence of config file
         Path pathToConfigFile = Paths.get(PathSupport.getPathToTestConfigForMockResources().toString(), testConfigFile);
         Assert.assertTrue(Files.exists(pathToConfigFile));
+
         // Prepare new configuration
         ConfigurationManager configurationManager = MockRegistry.findConfigurationManager();
         Configurations configurations = configurationManager.getConfigurations();
+        List<Configuration> savedConfigurations = new ArrayList<>();
+        savedConfigurations.addAll(configurations.getConfigurations());
+        LOGGER.debug("Saved size: {}", savedConfigurations.size());
         configurations.getConfigurations().clear();
         configurations.getConfigurations().addAll(this.loadTestConfiguration(pathToConfigFile).getConfigurations());
+
         // Request and assert
         Exchange response = getProducerTemplate().request("jetty:http://localhost:" + getAvailablePort() + "/mock/cmd", exchange -> {
             exchange.getIn().setHeader(Exchange.HTTP_METHOD, ConfigurationManager.HTTP_GET);
@@ -99,6 +89,11 @@ public class ConfigCommandTest extends RouteTestSupport {
         String actualConfig = response.getOut().getBody(String.class);
         String expectedConfig = new String(Files.readAllBytes(pathToConfigFile));
         assertXMLEqual(expectedConfig, actualConfig);
+
+        // Revert config (as we extend from a shared MockRegistry, and thus the same ConfigurationManager)
+        LOGGER.debug("Saved size: {}", savedConfigurations.size());
+        configurations.getConfigurations().clear();
+        configurations.getConfigurations().addAll(savedConfigurations);
     }
 
     private Configurations loadTestConfiguration(Path pathToConfigFile) throws Exception {
