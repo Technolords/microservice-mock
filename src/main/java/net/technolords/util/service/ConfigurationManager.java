969d442c24edf839ef4fcdebd31394830f16e581
Mike Ahlers <mike@technolords.net>
2022-11-20 15:38:15 +0100

    Added validation

--- src/main/java/net/technolords/util/service/ConfigurationManager.java
+++ src/main/java/net/technolords/util/service/ConfigurationManager.java
@@ -5,9 +5,18 @@ import org.apache.camel.VetoCamelContextStartException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
+import org.xml.sax.SAXException;
 
 import javax.annotation.PostConstruct;
+import javax.xml.XMLConstants;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -15,6 +24,7 @@ import java.nio.file.Paths;
 @Service
 public class ConfigurationManager {
     private static final Logger LOGGER = LoggerFactory.getLogger(ConfigurationManager.class);
+    private static final String PATH_TO_SCHEMA_FILE = "xsd/configuration.xsd";
 
     @Value("${camel.boot.config-file::config.xml}")
     private String pathToConfigFile;
@@ -26,6 +36,24 @@ public class ConfigurationManager {
             throw new VetoCamelContextStartException("config file does not exist: " + this.pathToConfigFile, camelContext);
         }
         LOGGER.info("Config file exists -> proceeding with XSD validation...");
+        try {
+            this.validateConfigFile(path);
+        } catch (SAXException | IOException e) {
+            throw new VetoCamelContextStartException(e.getMessage(), camelContext);
+        }
+        LOGGER.info("Config file is valid -> proceeding with loading endpoints...");
+    }
+
+    protected void validateConfigFile(Path pathToConfigFile) throws SAXException, IOException {
+        LOGGER.info("About to validate the configuration...");
+        SchemaFactory schemaFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+        ClassPathResource classPathResource = new ClassPathResource(PATH_TO_SCHEMA_FILE);
+        Source xsdSource = new StreamSource(classPathResource.getInputStream());
+        Schema schema = schemaFactory.newSchema(xsdSource);
+        Validator validator = schema.newValidator();
+        Source sourceToConfig = new StreamSource(pathToConfigFile.toFile());
+        validator.validate(sourceToConfig);
+        LOGGER.info("... valid, proceeding...");
     }
 
     @PostConstruct
